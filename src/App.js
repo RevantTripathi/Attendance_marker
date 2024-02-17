5b42503a11dcd8698e746c8c0662b62cced100d8
Revant Tripathi <revanttripathi8@gmail.com>
2024-02-17 23:26:59 +0530

    login-done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from './login/login.js';
 function App() {
   return (
     <div className="App">
-       <p>Hello</p>
+       
        <Login/>
     </div>
   );
--- src/login/login.js
+++ src/login/login.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
+import './login.css'
 
+function Login() {
+    return (
 
-function Login(){
-    return(
-    <>
-   <div className='login_div'>
-    
-   </div>
-    </>
+        <div className='login_div'>
+            <div className='login'>
+                <div className='wl_text'>
+                    <h1>Welcome Back</h1>
+                </div>
+                <div className='sec'>
+                    {/* <label>Email</label> */}
+                    <input placeholder='Email' type='text'></input>
+                </div>
+                <div className='sec2'>
+                    {/* <label>Password</label> */}
+                    <input placeholder='Password' type='password'></input>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
