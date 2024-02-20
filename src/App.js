a17af0bee065e8f6004ec1fea25c73a481dcefe6
Revant Tripathi <revanttripathi8@gmail.com>
2024-02-20 23:21:00 +0530

    time_home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './login/login.js';
 import Regi from './regi/regi.js';
+import Home from './Home/home.js';
 
 function App() {
   return (
     <div className="App">
 
-      <Login />
+      {/* <Login /> */}
       {/* <Regi />   */}
+      <Home/>
     </div>
   );
 }
---
+++ src/Home/home.js
@@ -0,0 +1,25 @@
+import React, { useState,useEffect } from 'react'
+import "./home.css"
+
+
+function Home(){
+    var [date,setDate] = useState(new Date());
+    
+    useEffect(() => {
+        var timer = setInterval(()=>setDate(new Date()), 1000 )
+        return function cleanup() {
+            clearInterval(timer)
+        }
+    
+    });
+    return(
+        <div className='home'>
+            <div className='time'>
+                <h1>{date.toLocaleTimeString()}</h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
