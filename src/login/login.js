15f3250ae23aca9b5f78a552cf6a6e134fc10d00
Revant Tripathi <revanttripathi8@gmail.com>
2024-02-20 21:48:18 +0530

    design_change

--- src/login/login.js
+++ src/login/login.js
@@ -8,17 +8,19 @@ function Login() {
             <div className='login'>
                 <div className='wl_text1'>
                     <h1>Welcome Back</h1>
+                    <p>Glad to see you again</p>
+                    <p>Login to your account bellow</p>
                 </div>
                 <div className='sec'>
                     {/* <label>Email</label> */}
-                    <input placeholder='Email' type='text'></input>
+                    <input className="I1" placeholder='Email' type='text' required></input>
                 </div>
                 <div className='sec2'>
                     {/* <label>Password</label> */}
-                    <input placeholder='Password' type='password'></input>
+                    <input className="I1" placeholder='Password' type='password' required></input>
                 </div>
                 <button className='b'>Login</button>
-                <button className='bb'>Don't have a account? Register</button>
+                <button className='bb'>Don't have a account? <div className='r'><b>Register</b></div></button>
             </div>
         </div>
 
