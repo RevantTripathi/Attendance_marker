430b04136a2aa9b924b4061adb837a6e5c9e148c
Revant Tripathi <revanttripathi8@gmail.com>
2024-02-23 21:59:02 +0530

    avatar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import Login from './login/login.js';
 import Regi from './regi/regi.js';
 import Home from './Home/home.js';
+import Avatar from './Home/avatar.jpg';
 
 function App() {
   return (
     <div className="App">
 
-      <Login /> 
-       <Regi />  
-      {/* <Home/> */}
+      {/* <Login /> 
+       <Regi />   */}
+      <Home src={Avatar} alt="avatar" />
     </div>
   );
 }
--- src/Home/home.js
+++ src/Home/home.js
@@ -1,19 +1,32 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./home.css"
+import Empty from "./empty.jpeg"
 
 
-function Home(){
-    var [date,setDate] = useState(new Date());
-    
+function Home({ src, alt }) {
+    var [date, setDate] = useState(new Date());
+
     useEffect(() => {
-        var timer = setInterval(()=>setDate(new Date()), 1000 )
+        var timer = setInterval(() => setDate(new Date()), 1000)
         return function cleanup() {
             clearInterval(timer)
         }
-    
+
     });
-    return(
+    return (
         <div className='home'>
+            <div className='profile'>
+                {src ? (
+                    <img className="icon" src={src} alt={alt} />
+                ) : (
+                    <img className="icon2" src={Empty} alt={alt} />
+                )}
+                <div className='profile_details'>
+                    <h2>Revant<br/>
+                    Front-end Developer</h2>
+                </div>
+            </div>
+
             <div className='time'>
                 <h1>{date.toLocaleTimeString()}</h1>
                 <h1>{date.toLocaleDateString()}</h1>
